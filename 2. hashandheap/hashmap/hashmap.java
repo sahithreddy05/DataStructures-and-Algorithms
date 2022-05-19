fed46a7672d943ff236696d5e2c60d9e26bd505c
sahithreddy05 <sahithreddy050101@gmail.com>
2022-05-19 12:34:28 +0530

    update

--- 2. hashandheap/hashmap/hashmap.java
+++ 2. hashandheap/hashmap/hashmap.java
@@ -1,80 +1,71 @@
 import java.util.ArrayList;
 
 public class hashmap {
-
-    private class Node {
+    private class Node{
         Integer key = 0;
         Integer value = 0;
         Node next = null;
 
-        Node(Integer key, Integer value) {
+        Node(Integer key,Integer value){
             this.key = key;
             this.value = value;
         }
     }
 
-    private class linkedlist {
-        public Node head = null;
-        public Node tail = null;
+    private class linkedlist{
+        public node head = null;
+        public node tail = null;
         public int NoOfElements = 0;
-
-        public linkedlist() {
+        
+        public linkedlist(){
             this.head = this.tail = null;
             this.NoOfElements = 0;
         }
 
-        public int size() {
-            return this.NoOfElements;
-        }
-
-        public void addLast(Node node) {
-            if (this.head == null)
-                head = tail = node;
-            else {
-                this.tail.next = node;
-                this.tail = node;
-            }
-        }
-
-        public int getFirst() {
-            return this.head.key;
-        }
-
-        public Node removeFirst() {
-            Node node = this.head;
-            if (this.NoOfElements == 0)
-                head = tail = null;
-            else {
-                head = node.next;
-                node.next = null;
-            }
-            NoOfElements--;
-            return node;
-        }
+    public int size(){
+        return NoOfElements;
     }
+    public void addLast(Node node){
+        if(head == null) head = tail = node;
+        else{
+            this.tail.next = node;
+            this.tail = node;
+        }
+        NoOfElements++;
+    }
+
+    public int getFirst(){
+        return this.head.key;
+    }
+    public Node removeFirst(){
+        Node node = this.head;
+        if(this.NoOfElements == 0) head  = tail = null;
+        else{
+            this.head  = node.next;
+            node.next = null;
+        }
+        NoOfElements--;
+        return node;
+    }
+}
 
     private linkedlist[] containers;
     private int sizeOfHM = 0;
-
     public void assignValues(int size) {
         containers = new linkedlist[size];
-        for (int i = 0; i < size; i++) {
+        for(int i=0;i<size;i++){
             containers[i] = new linkedlist();
         }
     }
 
-    hashmap() {
-        assignValues(10);
-    }
-    
-
-    private linkedlist group(Integer key) {
+    private linkedlist group(Integer key){
         int code = hashCode(key);
         return this.containers[code];
     }
-
-    private int hashCode(Integer key) {
+    private int hashCode(Integer key){
         int value = key.hashCode();
         return value % containers.length;
     }
+
+
 }
--- 8.DP/l006_cutType.java
+++ 8.DP/l006_cutType.java
@@ -14,100 +14,169 @@ public class l006_cutType {
     }
     // 312 burst ballons
 
-    public int maxCoins(int[] nums,int si,int ei,int[][] dp){
-        if(dp[si][ei] != 0) return dp[si][ei];
+    public int maxCoins(int[] nums, int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
         int lele = si == 0 ? 1 : nums[si - 1];
-        int rele = ei == nums.length - 1 ? 1:nums[ei + 1];
-            
+        int rele = ei == nums.length - 1 ? 1 : nums[ei + 1];
+
         int maxCoins = 0;
-        for(int cut = si;cut <= ei;cut++)
-        {
-            int lcost = cut == si ? 0 :  maxCoins(nums,si,cut - 1,dp);
-            int rcost = cut == ei ? 0 :  maxCoins(nums,cut + 1,ei,dp);
-            
-            maxCoins = Math.max(maxCoins,lcost + lele * nums[cut] * rele + rcost);
+        for (int cut = si; cut <= ei; cut++) {
+            int lcost = cut == si ? 0 : maxCoins(nums, si, cut - 1, dp);
+            int rcost = cut == ei ? 0 : maxCoins(nums, cut + 1, ei, dp);
+
+            maxCoins = Math.max(maxCoins, lcost + lele * nums[cut] * rele + rcost);
         }
-        
+
         return dp[si][ei] = maxCoins;
-        
+
     }
+
     public int maxCoins(int[] nums) {
-      int n = nums.length;
+        int n = nums.length;
         int[][] dp = new int[n][n];
-        
-        return maxCoins(nums,0,n-1,dp);
+
+        return maxCoins(nums, 0, n - 1, dp);
+    }
+
+    // min and max
+    public static class pairmm {
+        int min = (int) 1e9;
+        int max = 0;
+
+        pairmm() {
+
+        }
+
+        pairmm(int val) {
+            this.min = this.max = val;
+        }
+    }
+
+    public static pairmm evaluateMinMax(pairmm leftRes, pairmm rightRes, char operator) {
+
+        pairmm pair = new pairmm();
+        if (operator == '+') {
+            pair.min = leftRes.min + rightRes.min;
+            pair.max = leftRes.max + rightRes.max;
+        } else if (operator == '*') {
+            pair.min = leftRes.min * rightRes.min;
+            pair.max = leftRes.max * rightRes.max;
+        }
+        return pair;
+    }
+
+    public static pairmm minMax(String str, int si, int ei, pairmm[][] dp) {
+
+        if (si == ei) {
+            return dp[si][ei] = new pairmm((str.charAt(si) - '0'));
+        }
+
+        if (dp[si][ei] != null) {
+            return dp[si][ei];
+        }
+
+        pairmm myRes = new pairmm();
+        for (int cut = si + 1; cut < ei; cut += 2) {
+
+            pairmm leftRes = minMax(str, si, cut - 1, dp);
+            pairmm rightRes = minMax(str, cut + 1, ei, dp);
+            pairmm pair = evaluateMinMax(leftRes, rightRes, str.charAt(cut));
+
+            myRes.min = Math.min(myRes.min, pair.min);
+            myRes.max = Math.max(myRes.max, pair.max);
+        }
+
+        return dp[si][ei] = myRes;
+    }
+
+    public static void minMax() {
+        String str = "1+2*3+4*5";
+        int n = str.length();
+        pairmm[][] dp = new pairmm[n][n];
+
+        pairmm res = minMax(str, 0, n - 1, dp);
+
+        System.out.println("Min value: " + res.min);
+        System.out.println("Max value: " + res.max);
     }
 
     // boolean parentization
-    public static class pairBoolean{
+    public static class pairBoolean {
         long totalTrue = 0;
         long totalFalse = 0;
-        
-        pairBoolean(){
-            
+
+        pairBoolean() {
+
         }
-        pairBoolean(long totalTrue,long totalFalse){
+
+        pairBoolean(long totalTrue, long totalFalse) {
             this.totalTrue = totalTrue;
             this.totalFalse = totalFalse;
         }
     }
-    
-    public static void evaluateTrue(pairBoolean left,pairBoolean right,pairBoolean res,char operator){
-        
+
+    public static void evaluateTrue(pairBoolean left, pairBoolean right, pairBoolean res, char operator) {
+
         long mod = 1003;
         long totalTF = (left.totalTrue + left.totalFalse) * (right.totalTrue + right.totalFalse) % mod;
         long totalTrue = 0, totalFalse = 0;
-        
-        if(operator == '|'){
-            
-         totalFalse = (left.totalFalse * right.totalFalse) % mod;
-         totalTrue = (totalTF - totalFalse + mod) % mod;    
-         
-        }else if(operator == '^'){
-            
-         totalTrue = (left.totalFalse * right.totalTrue) % mod + (left.totalTrue * right.totalFalse) % mod;
-         totalFalse = (totalTF - totalTrue + mod) % mod;
-         
-        }else if(operator == '&'){
 
-        totalTrue = (left.totalTrue * right.totalTrue) % mod;
-        totalFalse = (totalTF - totalTrue + mod) % mod; 
-        
-    }
-        
+        if (operator == '|') {
+
+            totalFalse = (left.totalFalse * right.totalFalse) % mod;
+            totalTrue = (totalTF - totalFalse + mod) % mod;
+
+        } else if (operator == '^') {
+
+            totalTrue = (left.totalFalse * right.totalTrue) % mod + (left.totalTrue * right.totalFalse) % mod;
+            totalFalse = (totalTF - totalTrue + mod) % mod;
+
+        } else if (operator == '&') {
+
+            totalTrue = (left.totalTrue * right.totalTrue) % mod;
+            totalFalse = (totalTF - totalTrue + mod) % mod;
+
+        }
+
         res.totalFalse = (res.totalFalse + totalFalse) % mod;
         res.totalTrue = (res.totalTrue + totalTrue) % mod;
-        
+
     }
-    
-    
-    public static pairBoolean countWays(String S,int si,int ei,pairBoolean[][] dp){
-        if(si == ei){
+
+    public static pairBoolean countWays(String S, int si, int ei, pairBoolean[][] dp) {
+        if (si == ei) {
             char ch = S.charAt(si);
             int t = ch == 'T' ? 1 : 0;
             int f = ch == 'F' ? 1 : 0;
-            return dp[si][ei] = new pairBoolean(t,f);
+            return dp[si][ei] = new pairBoolean(t, f);
         }
-        
-        if(dp[si][ei] != null){
+
+        if (dp[si][ei] != null) {
             return dp[si][ei];
         }
         pairBoolean res = new pairBoolean();
-        for(int cut = si+1;cut<ei;cut+=2){
-            
-        pairBoolean lres = countWays(S,si, cut - 1,dp);
-        
-        pairBoolean rres = countWays(S,cut + 1,ei,dp);
-        
-        evaluateTrue(lres,rres,res,S.charAt(cut));
-        
+        for (int cut = si + 1; cut < ei; cut += 2) {
+
+            pairBoolean lres = countWays(S, si, cut - 1, dp);
+
+            pairBoolean rres = countWays(S, cut + 1, ei, dp);
+
+            evaluateTrue(lres, rres, res, S.charAt(cut));
+
         }
-        
+
         return dp[si][ei] = res;
-    } 
-    static int countWays(int N, String S){
+    }
+
+    static int countWays(int N, String S) {
         // code here
         pairBoolean[][] dp = new pairBoolean[N][N];
-        return (int) countWays(S,0,N-1,dp).totalTrue;
+        return (int) countWays(S, 0, N - 1, dp).totalTrue;
+    }
+
+
+    public static void main(String[] args) {
+        minMax();
     }
 }
